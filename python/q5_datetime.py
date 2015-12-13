30e179d9f4fd6485720154f74ee3ac33a44b3d97
David <daviddupuis@macbook-pro.home>
2015-12-13 19:37:23 +0100

    update q5_datetime.py for python version 3.4.3

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -7,14 +7,14 @@ def computeDays(start, stop,frmt):
     result = datetime.strptime(stop,frmt) - datetime.strptime(start,frmt);
     return "Days: " + str(result.days)
 
-print computeDays(date_start,date_stop,"%m-%d-%Y")
+print(computeDays(date_start,date_stop,"%m-%d-%Y"))
 
 date_start = '12312013'
 date_stop = '05282015'
 
-print computeDays(date_start,date_stop,"%m%d%Y")
+print(computeDays(date_start,date_stop,"%m%d%Y"))
 
 date_start = '15-Jan-1994'
 date_stop = '14-Jul-2015'
 
-print computeDays(date_start,date_stop,"%d-%b-%Y")
+print(computeDays(date_start,date_stop,"%d-%b-%Y"))
